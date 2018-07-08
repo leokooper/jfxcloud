7fcb7b65b93503b3d9ec52988923b868626dfb35
leokooper <lverixas@gmail.com>
2018-07-08 21:31:17 +0300

    //added hibernate and mysql connection

--- client/src/main/java/ru/jfxcloud/client/Main.java
+++ client/src/main/java/ru/jfxcloud/client/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("JFXCloud Client");
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
